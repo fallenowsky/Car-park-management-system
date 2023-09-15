570b44889428891e9ceab1e2e1187199479b5ef8
Marcin Miaso <marcin.miaso.programista@gmail.com>
2023-09-15 10:17:55 +0200

    initial commit

---
+++ src/main/java/pl/kurs/mmiaso/carparkapptest/CarParkAppTestApplication.java
@@ -0,0 +1,13 @@
+package pl.kurs.mmiaso.carparkapptest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarParkAppTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarParkAppTestApplication.class, args);
+	}
+
+}
