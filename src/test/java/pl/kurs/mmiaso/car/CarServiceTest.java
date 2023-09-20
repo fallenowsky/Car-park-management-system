fa8c0113d9c81b0eedb7b78f9359dae251e136e6
Marcin Miaso <marcin.miaso.programista@gmail.com>
2023-09-20 15:01:33 +0200

    added CarServiceTest without locking tests

--- src/test/java/pl/kurs/mmiaso/car/CarServiceTest.java
+++ src/test/java/pl/kurs/mmiaso/car/CarServiceTest.java
@@ -1,5 +1,6 @@
 package pl.kurs.mmiaso.car;
 
+import jakarta.persistence.EntityNotFoundException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -9,6 +10,9 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import pl.kurs.mmiaso.address.model.Address;
+import pl.kurs.mmiaso.car.exceptions.GarageIsFullWithCarsException;
+import pl.kurs.mmiaso.car.exceptions.GarageNotHandleLpgException;
+import pl.kurs.mmiaso.car.exceptions.GaragePlaceIsTooNarrowException;
 import pl.kurs.mmiaso.car.model.Car;
 import pl.kurs.mmiaso.car.model.dto.CarDto;
 import pl.kurs.mmiaso.fuel.FuelRepository;
@@ -17,12 +21,14 @@ import pl.kurs.mmiaso.garage.GarageRepository;
 import pl.kurs.mmiaso.garage.model.Garage;
 
 import java.math.BigDecimal;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -37,22 +43,29 @@ class CarServiceTest {
     private CarService service;
     @Captor
     private ArgumentCaptor<Car> carArgumentCaptor;
+    private Garage garage;
+    private Garage garage2;
     private Car car;
     private Car car2;
+    private Fuel fuel;
+    private Fuel fuel2;
     private CarDto carDto;
     private CarDto car2Dto;
     private long garageId;
+    private long fuelId;
 
     @BeforeEach
     public void init() {
-        Garage garage = new Garage(1L, 3, false, 2.5, 1L,
+        fuel = new Fuel(1L, "Petrol");
+        fuel2 = new Fuel(2L, "LPG");
+        garage = new Garage(1L, 3, false, 2.5, 1L,
                 new Address(1L, "fallenowsky", "Złota 44", "33-199", "Warszawa", "Polska", Collections.emptySet()),
                 Collections.emptySet());
-        Garage garage1 = new Garage(2L, 2, true, 3.5, 1L,
+        garage2 = new Garage(2L, 2, true, 3.5, 1L,
                 new Address(2L, "quarkowsky", "Srebrna 44", "33-199", "Poznań", "Polska", Collections.emptySet()),
                 Collections.emptySet());
-        car = new Car(1L, "BMW", 2.3, BigDecimal.valueOf(299000.4), new Fuel(1L, "Petrol"), garage);
-        car2 = new Car(2L, "LAMBORGHINI", 2.9, BigDecimal.valueOf(4324212.4), new Fuel(2L, "Electric"), garage1);
+        car = new Car(1L, "BMW", 2.3, BigDecimal.valueOf(299000.4), fuel, garage);
+        car2 = new Car(2L, "LAMBORGHINI", 2.9, BigDecimal.valueOf(4324212.4), fuel2, garage2);
         carDto = CarDto.builder()
                 .brand("BMW")
                 .width(2.3)
@@ -64,9 +77,108 @@ class CarServiceTest {
                 .brand("LAMBORGHINI")
                 .width(2.9)
                 .price(BigDecimal.valueOf(4324212.4))
-                .fuelName("Electric")
+                .fuelName("LPG")
                 .build();
         garageId = 1L;
+        fuelId = 1L;
+    }
+
+    @Test
+    public void testSave_MethodInputsCorrect_ResultsInMockMethodCallAndCarCapture() {
+        when(garageRepository.findWithLockingById(garageId)).thenReturn(Optional.of(garage));
+        when(fuelRepository.findById(fuelId)).thenReturn(Optional.of(fuel));
+        when(carRepository.findCarsAmountByGarageId(garageId)).thenReturn(2);
+
+        service.save(carDto, garageId, fuelId);
+
+        verify(garageRepository).findWithLockingById(garageId);
+        verify(fuelRepository).findById(fuelId);
+        verify(carRepository).save(carArgumentCaptor.capture());
+        Car carCaptured = carArgumentCaptor.getValue();
+        assertEquals(car.getBrand(), carCaptured.getBrand());
+        assertEquals(car.getGarage(), carCaptured.getGarage());
+        assertEquals(car.getFuel(), carCaptured.getFuel());
+        assertEquals(car.getPrice(), carCaptured.getPrice());
+        assertEquals(car.getWidth(), carCaptured.getWidth());
+    }
+
+    @Test
+    public void testSave_GarageIsFull_ResultsInGarageIsFullWithCarsException() {
+        String excMessage = "This garage is full!";
+        when(garageRepository.findWithLockingById(garageId)).thenReturn(Optional.of(garage));
+        when(fuelRepository.findById(fuelId)).thenReturn(Optional.of(fuel));
+        when(carRepository.findCarsAmountByGarageId(garageId)).thenReturn(3);
+
+        assertThatExceptionOfType(GarageIsFullWithCarsException.class)
+                .isThrownBy(() -> service.save(carDto, garageId, fuelId))
+                .withMessage(excMessage);
+
+        verify(garageRepository).findWithLockingById(garageId);
+        verify(fuelRepository).findById(fuelId);
+        verify(carRepository).findCarsAmountByGarageId(garageId);
+        verifyNoMoreInteractions(carRepository);
+    }
+
+    @Test
+    public void testSave_LpgInGarageNotAllowedCarIsLpg_ResultsInGarageNotHandleLpgException() {
+        String excMessage = "In this garage lpg is not allowed!";
+        when(garageRepository.findWithLockingById(garageId)).thenReturn(Optional.of(garage));
+        when(fuelRepository.findById(fuelId)).thenReturn(Optional.of(fuel2));
+        when(carRepository.findCarsAmountByGarageId(garageId)).thenReturn(2);
+
+        assertThatExceptionOfType(GarageNotHandleLpgException.class)
+                .isThrownBy(() -> service.save(car2Dto, garageId, fuelId))
+                .withMessage(excMessage);
+
+        verify(garageRepository).findWithLockingById(garageId);
+        verify(fuelRepository).findById(fuelId);
+        verify(carRepository).findCarsAmountByGarageId(garageId);
+        verifyNoMoreInteractions(carRepository);
+    }
+
+    @Test
+    public void testSave_CarIsTooWideForGaragePlace_ResultsInGarageIsGaragePlaceIsTooNarrowException() {
+        String excMessage = "Garage place is too narrow for your car!";
+        when(garageRepository.findWithLockingById(garageId)).thenReturn(Optional.of(garage));
+        when(fuelRepository.findById(fuelId)).thenReturn(Optional.of(fuel));
+        when(carRepository.findCarsAmountByGarageId(garageId)).thenReturn(2);
+        carDto.setWidth(2.51);
+
+        assertThatExceptionOfType(GaragePlaceIsTooNarrowException.class)
+                .isThrownBy(() -> service.save(carDto, garageId, fuelId))
+                .withMessage(excMessage);
+
+        verify(garageRepository).findWithLockingById(garageId);
+        verify(fuelRepository).findById(fuelId);
+        verify(carRepository).findCarsAmountByGarageId(garageId);
+        verifyNoMoreInteractions(carRepository);
+    }
+
+    @Test
+    public void testSave_FuelNotFound_ResultsInGarageIsEntityNotFoundException() {
+        String excMessage = "Fuel with id=1 not found";
+        when(garageRepository.findWithLockingById(garageId)).thenReturn(Optional.of(garage));
+
+        assertThatExceptionOfType(EntityNotFoundException.class)
+                .isThrownBy(() -> service.save(carDto, garageId, fuelId))
+                .withMessage(excMessage);
+
+        verify(garageRepository).findWithLockingById(garageId);
+        verify(fuelRepository).findById(fuelId);
+        verifyNoInteractions(carRepository);
+    }
+
+    @Test
+    public void testSave_GarageNotFound_ResultsInGarageIsEntityNotFoundException() {
+        String excMessage = "Garage with id=1 not found";
+
+        assertThatExceptionOfType(EntityNotFoundException.class)
+                .isThrownBy(() -> service.save(carDto, garageId, fuelId))
+                .withMessage(excMessage);
+
+        verify(garageRepository).findWithLockingById(garageId);
+        verifyNoInteractions(fuelRepository);
+        verifyNoInteractions(carRepository);
     }
 
     @Test
@@ -105,6 +217,5 @@ class CarServiceTest {
         verify(carRepository).findAllByGarageIdWithFuelJoin(garageId);
         verifyNoMoreInteractions(carRepository);
     }
-
 
 }
